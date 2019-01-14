585c76b4e1438e8fa3c46448961d9cafb993bd6a
Stephen Chu <stephen.chu530@gmail.com>
2019-01-14 08:29:23 -0800

    feat: initial commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Prepares the web server for deployment of a static web page
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/
+mkdir -p /data/web_static
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/test/
+echo "Test HTML Text" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu:ubuntu /data/
+SRC=https://raw.githubusercontent.com/stephenchu530/AirBnB_clone_v2/master/default
+DST=/etc/nginx/sites-available/default
+wget $SRC -O $DST
