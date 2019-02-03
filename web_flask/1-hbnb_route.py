0c7894878dfa64dad50b5c30cc8b0af43c98f035
Francesca <fpcantor@gmail.com>
2019-02-03 23:15:01 +0000

    Initial commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Script that starts a Flask web application '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    ''' Returns a phrase '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' Returns phrase "HBNB" '''
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run()
