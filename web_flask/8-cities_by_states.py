296255d331f5278ccf9bf25a6216087857df747b
Francesca <fpcantor@gmail.com>
2019-02-04 06:46:30 +0000

    Removed print statements

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -29,8 +29,6 @@ def cities_by_states():
             cities_list[v.state_id].append([v.id, v.name])
         else:
             cities_list[v.state_id] = [[v.id, v.name]]
-    print("STATES: {}".format(states_list))
-    print("CITIES: {}".format(cities_list))
     return render_template('8-cities_by_states.html', states_list=states_list,
                            cities_list=cities_list)
 
