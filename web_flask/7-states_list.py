d8c9cfc1e2d8e0aa20127f57e2565b7d18f18d01
Francesca <fpcantor@gmail.com>
2019-02-04 06:00:37 +0000

    Initial commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' Script that starts a Flask web application '''
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    ''' Returns an HTML page '''
+    states = storage.all("State")
+    states_list = []
+    for state in states.values():
+        states_list.append([state.id, state.name])
+    return render_template('7-states_list.html', states_list=states_list)
+
+@app.teardown_appcontext
+def teardown(exception):
+    ''' Closes the database '''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run()
