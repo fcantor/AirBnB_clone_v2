f357b4fb51a693bb02c9059c46c9f848c52a503a
Francesca <fpcantor@gmail.com>
2019-02-04 08:28:49 +0000

    Fixed for loop in city loop

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def show_cities_by_states():
     ''' Returns an HTML page showing states and its cities '''
-    data = storage.all('State')
-    states = []
-    for k, v in data.items():
-        states.append(v)
-    return render_template('8-cities_by_states.html', states=states)
+    obj = storage.all('State')
+    states_list = []
+    for k, v in obj.items():
+        states_list.append(v)
+    return render_template('8-cities_by_states.html', states_list=states_list)
 
 
 @app.teardown_appcontext
-def remove_session(response_or_exc):
+def remove_session(exception):
     ''' Closes the current storage session '''
     storage.close()
 
