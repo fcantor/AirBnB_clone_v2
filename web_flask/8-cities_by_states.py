19c353c208f633b1cb42691bc7607fd37b1f4770
Francesca <fpcantor@gmail.com>
2019-02-04 08:09:52 +0000

    Fixed variable names

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,19 +6,18 @@ app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    ''' Returns an HTML page with cities and states '''
-    storage.reload()
-    states = storage.all("State")
-    states_list = []
-    for k, v in states.items():
-        states_list.append(v)
-    return render_template('8-cities_by_states.html', states_list=states_list)
+def show_cities_by_states():
+    ''' Returns an HTML page showing states and its cities '''
+    data = storage.all('State')
+    states = []
+    for k, v in data.items():
+        states.append(v)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    ''' Closes the database '''
+def remove_session(response_or_exc):
+    ''' Closes the current storage session '''
     storage.close()
 
 
