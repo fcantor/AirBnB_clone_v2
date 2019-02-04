f4d61135030fa88346327084404e057928f627e7
Francesca <fpcantor@gmail.com>
2019-02-04 02:05:59 +0000

    Changed parameters

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
+def number_template(n=None):
     ''' Displays an HTML page if n is an integer '''
     if isinstance(n, int):
         return render_template('5-number.html', n=n)
